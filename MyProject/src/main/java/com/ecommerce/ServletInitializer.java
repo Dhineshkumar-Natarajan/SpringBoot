92bf468cc65402fa6e2b1bb991f59fe14f29369b
hp <hp@DHINESHKUMAR>
2023-11-09 03:54:03 +0530

    file renamed

--- MyProject/src/main/java/com/ecommerce/ServletInitializer.java
+++ MyProject/src/main/java/com/ecommerce/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(MyProjectApplication.class);
+		return application.sources(EcommerceApplication.class);
 	}
 
 }
