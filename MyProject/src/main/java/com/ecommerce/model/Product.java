a87f6af644bf40ebfcee037aa97d1ca1180dc864
hp <hp@DHINESHKUMAR>
2023-11-09 03:48:34 +0530

    added classes

--- MyProject/src/main/java/com/ecommerce/model/Product.java
+++ MyProject/src/main/java/com/ecommerce/model/Product.java
@@ -1,5 +1,56 @@
 package com.ecommerce.model;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "PRODUCT")
 public class Product {
+
+	    @Id
+	    private Long id;
+
+	    @NotNull(message = "Product name is required.")
+	    @Basic(optional = false)
+	    private String name;
+
+	    private Double price;
+
+	    private String pictureUrl;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public Double getPrice() {
+			return price;
+		}
+
+		public void setPrice(Double price) {
+			this.price = price;
+		}
+
+		public String getPictureUrl() {
+			return pictureUrl;
+		}
+
+		public void setPictureUrl(String pictureUrl) {
+			this.pictureUrl = pictureUrl;
+		}
 
 }
